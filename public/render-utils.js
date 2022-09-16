a5893f48e1686520dd29b0ca3ec1d4d07598ca58
gftruj <gftruj@gmail.com>
2022-09-16 20:53:12 +0200

    expose the glb export

--- public/render-utils.js
+++ public/render-utils.js
@@ -1,30 +1,30 @@
+function exportScene() {
+    const scene = document.querySelector('a-scene');
+    if (!scene) throw console.error("no scene");
+
+    const exporter = new THREE.GLTFExporter();
+    exporter.parse(
+        scene.object3D,
+        // called when the gltf has been generated
+        function (gltf) {
+            electron.saveGLB(gltf);
+        },
+        // called when there is an error in the generation
+        function (error) {
+            console.log('An error happened', error);
+        },
+        { binary: true }
+    );
+}
 
 function createGLTFExportBtn() {
     const glte = document.createElement("button");
     glte.innerHTML = "export scene as glb"
-    glte.addEventListener("click", () => {
-        const scene = document.querySelector('a-scene');
-        if (!scene) throw console.error("no scene");
-
-        const exporter = new THREE.GLTFExporter();
-        exporter.parse(
-            scene.object3D,
-            // called when the gltf has been generated
-            function ( gltf ) {
-                console.log(gltf)
-                electron.saveGLB(gltf);
-            },
-            // called when there is an error in the generation
-            function ( error ) {
-                console.log( 'An error happened', error );
-            },
-            {binary: true}
-        );
-    });
+    glte.addEventListener("click", exportScene);
     return glte;
 }
 
-(function() {
+(function () {
     /* Create UI*/
     const div = document.createElement("div")
     const perspBtn = document.createElement("button");
@@ -44,9 +44,9 @@ function createGLTFExportBtn() {
     function render(_projection) {
         const projection = _projection === "perspective" ? "perspective" : "equirectangular"
         const scene = document.querySelector('a-scene');
-        const canvas = scene.components.screenshot.getCanvas(projection); 
+        const canvas = scene.components.screenshot.getCanvas(projection);
         const dataURL = canvas.toDataURL("image/png");
-        electron.savePNG({projection: projection, dataUrl: dataURL})
+        electron.savePNG({ projection: projection, dataUrl: dataURL })
     }
 
     window.renderPerspective = render.bind(this, "perspective")
@@ -59,13 +59,15 @@ function createGLTFExportBtn() {
     if (!THREE.GLTFExporter) {
         var script = document.createElement('script');
         script.onload = function () {
+            window.exportScene = exportScene;
             glte = createGLTFExportBtn();
             div.appendChild(glte);
         };
-        script.src = "https://threejs.org/examples/js/exporters/GLTFExporter.js"; 
+        script.src = "https://threejs.org/examples/js/exporters/GLTFExporter.js";
         document.head.appendChild(script); //or something of the likes
     } else {
         glte = createGLTFExportBtn();
+        window.exportScene = exportScene;
         div.appendChild(glte);
     }
 
